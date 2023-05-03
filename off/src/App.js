91d23598a9e6947eca9179b0cc307f5e1703a4c4
caitlynw5 <widjajacaitlyn@gmail.com>
2023-05-02 18:30:15 -0700

    search bar
    search bar stuffs

--- off/src/App.js
+++ off/src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import SearchBar from './components/searchBar'
+
 
 function App() {
   return (
     <header>
       <p>Our Food Family</p>
     </header>
+
+// not sure what stuff below does but i need it for search bar
+/*
+<div classname='App'>
+  <SearchBar/>
+</div>
+*/
   );
 }
 
